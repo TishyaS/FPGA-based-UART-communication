c82cdb2cdc75a8d236c0d0fddf6583f18aa71e70
Tishya Sharma Sarkar <105290202+TishyaS@users.noreply.github.com>
2022-06-21 13:35:32 +0530

    Add files via upload

---
+++ Tx_Rx_selection.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Tishya Sharma Sarkar
+-- 
+-- Create Date: 25.02.2022 18:20:58
+-- Design Name: 
+-- Module Name: Tx_Rx_selection - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Tx_Rx_selection is
+    Port ( Gpio : out STD_LOGIC_VECTOR(1 DOWNTO 0);
+           Clock : in STD_LOGIC;
+           Resetn : in STD_LOGIC;
+           LED1 : in STD_LOGIC;
+           LED_D : in STD_LOGIC);
+end Tx_Rx_selection;
+
+architecture Behavioral of Tx_Rx_selection is
+
+signal gpio_input : std_logic_vector(1 downto 0);
+
+begin
+
+Gpio <= gpio_input;
+
+process(Clock)
+begin
+if(rising_edge(Clock)) then
+if(Resetn = '0') then
+gpio_input <= (others => '0');
+else
+gpio_input(1) <= LED_D;
+gpio_input(0) <= LED1;
+end if;
+end if;
+end process;
+
+end Behavioral;
---
+++ uart_application.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xuartps.h"
+#include "xuartlite.h"
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xil_types.h"
+
+#define NUM_OF_BYTE 1
+
+XUartPs_Config *Config_0;
+XUartPs Uart_PS_0;
+XUartLite_Config *Config_1;
+XUartLite UartLite_0;
+XGpio Gpio_0;
+
+int main()
+{
+    init_platform();
+    int Status;
+	u32 switch_data;
+	u8 BufferPtr_tx[1024];
+	u8 BufferPtr_rx[1024];
+	/*************************
+	 * UARTPS initialization *
+	 *************************/
+	Config_0 = XUartPs_LookupConfig(XPAR_XUARTPS_0_DEVICE_ID);
+	if (NULL == Config_0) {
+		return XST_FAILURE;
+	}
+	Status = XUartPs_CfgInitialize(&Uart_PS_0, Config_0, Config_0->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+	/*************************
+	 * UARTLite initialization *
+	 *************************/
+	Status = XUartLite_Initialize(&UartLite_0, XPAR_UARTLITE_0_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+	/*************************
+	 * GPIO initialization *
+	 *************************/
+	Status = XGpio_Initialize(&Gpio_0, XPAR_GPIO_0_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	XGpio_SetDataDirection(&Gpio_0, 1, 0xF);
+
+	while(1){
+		switch_data = XGpio_DiscreteRead(&Gpio_0, 1);
+		if(switch_data == 0b01){
+			Status = 0;
+			while (Status<NUM_OF_BYTE) {
+				Status +=	XUartPs_Recv(&Uart_PS_0, BufferPtr_tx, (NUM_OF_BYTE-Status));
+			}
+			XUartLite_Send(&UartLite_0, BufferPtr_tx, Status);
+			xil_printf("%s",BufferPtr_tx);
+		}
+		else if(switch_data == 0b10){
+			Status = 0;
+			while (Status<NUM_OF_BYTE) {
+				Status +=	XUartLite_Recv(&UartLite_0, BufferPtr_rx, (NUM_OF_BYTE - Status));
+			}
+			XUartPs_Send(&Uart_PS_0, BufferPtr_rx, Status);
+		}
+	}
+    cleanup_platform();
+    return 0;
+}
